9a481e576d45236f94d1c386ad5627b7d9610aac
unknown <mkmladja>
2018-09-19 15:25:24 +0200

    Added res

--- server.js
+++ server.js
@@ -37,6 +37,7 @@ if (town.isMaster) {
     app.use(function(req, res, next) {
         res.header("Access-Control-Allow-Origin", "*");
         res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        res.header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
         next();
     });
 
